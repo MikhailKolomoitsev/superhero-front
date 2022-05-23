122b466684dfd208adb2bb51c08fa3892f85fbe0
Mihon <75487015+MikhailKolomoitsev@users.noreply.github.com>
2022-05-23 10:17:26 +0000

    create

--- src/pages/CreateHero/CreateHero.jsx
+++ src/pages/CreateHero/CreateHero.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
-import './CreateHero.scss'
+import React, { useRef, useState } from 'react'
 import { Formik, Form } from 'formik';
 import { TextField, Box, Button } from '@mui/material';
 import { createHeroValidationSchema } from './validations';
+import './CreateHero.scss'
 
 const CreateHero = () => {
+    const [files, setFiles] = useState([]);
 
     const onSubmit = (data) => {
         console.log(data)
     }
+    const photoInput = useRef(null);
+    const handlePhotoClick = () => {
+        photoInput.current.click();
+    };
+    const handleFilesChange = (event) => {
+        console.log('works');
+        if (event.target.files[0].size > 159061428) {
+            alert('File is too big (more than 150mb)');
+            return;
+        }
+        if (files.length < 5) {
+            setFiles((prev) => [...prev, event.target.files[0]]);
+        } else {
+            alert('Cannot attach more than 5 files');
+        }
+    };
     return (
         <Formik
             initialValues={{
@@ -22,10 +39,9 @@ const CreateHero = () => {
             onSubmit={onSubmit}
         >
             {({ errors, touched, handleChange, setFieldValue, values }) => (
-                <Form className="">
+                <Form className="create-hero_form">
                     <Box sx={{ width: 500, maxWidth: '100%', margin: 1 }}>
                         <TextField
-                            defaultValue={'Has to be minimum 2 signs'}
                             onChange={handleChange}
                             fullWidth
                             id="nickname"
@@ -68,8 +84,27 @@ const CreateHero = () => {
                             required
                         />
                     </Box>
-
-                    <Button variant="contained" type='submit'>Contained</Button>
+                    <button
+                        onClick={handlePhotoClick}
+                        type="button"
+                        className="media-button"
+                    >Attach Photo
+                        <input
+                            onChange={(event) => {
+                                console.log('hadnle change')
+                                handleFilesChange(event);
+                                setFieldValue('files', values.file.concat(event.target.files));
+                            }}
+                            multiple
+                            ref={photoInput}
+                            id="files"
+                            name="files"
+                            type="files"
+                            accept="image/*"
+                            className="media-button_input"
+                        />
+                    </button>
+                    <Button color="secondary" type='submit'>Create Hero</Button>
                 </Form>
             )}
         </Formik>
