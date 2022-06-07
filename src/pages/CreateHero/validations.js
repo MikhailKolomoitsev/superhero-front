7e08dfd62c7a6302f7fad98685676ce3d3e40c42
Mihon <75487015+MikhailKolomoitsev@users.noreply.github.com>
2022-06-07 15:23:11 +0000

    ts template is ready

--- src/pages/CreateHero/validations.js
+++
@@ -1,9 +0,0 @@
-import * as yup from 'yup';
-
-export const createHeroValidationSchema = yup
-    .object().shape({
-        nickname: yup.string().min(2).max(50),
-        realName: yup.string().min(2).max(50),
-        superpowers: yup.string().min(3).max(550),
-        catchPhrase: yup.string().min(2).max(100),
-})
